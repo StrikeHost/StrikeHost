9df5010401cea8b5cc482f947ff00ed8e709f566
Adam Marshall <adamjmarshall275@gmail.com>
2022-04-08 16:14:50 +0100

    Add cpus to resource allocations

--- backend/src/instance/instance.repository.ts
+++ backend/src/instance/instance.repository.ts
@@ -26,12 +26,14 @@ export class InstanceRepository extends Repository<Instance> {
   ): Promise<Instance> {
     const { game_id, image_id, version_id } = createInstanceDto;
 
+    const requestedCpus = 1;
     const requestedMemory = 1024;
     const requestedStorage = 10;
 
     const instance = new Instance();
     instance.image = await this.imageRepository.findOne(image_id);
     instance.user = user;
+    instance.cpus = requestedCpus;
     instance.memory = requestedMemory;
     instance.storage = requestedStorage;
     instance.version = await this.imageVersionRepository.findOne(version_id);
@@ -43,6 +45,12 @@ export class InstanceRepository extends Repository<Instance> {
     agent.claimPort(instance.port);
     agent.allocated_memory += requestedMemory;
     agent.free_memory -= requestedMemory;
+
+    agent.allocated_cores += requestedCpus;
+    agent.free_cores -= requestedCpus;
+
+    agent.allocated_storage += requestedStorage;
+    agent.free_storage -= requestedStorage;
 
     await instance.save();
     await agent.save();
