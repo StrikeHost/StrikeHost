a7bf468af9e0b0b9302f68428f8ad666c5a4328e
Sam Gunner <me@sgunner.me>
2022-04-25 21:21:01 +0100

    CI issues

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Bugsnag from "@bugsnag/js";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
-import BugsnagPluginReact from "@bugsnag/plugin-react";
 
 import App from "./App";
 import { rootReducer } from "redux/reducers/RootReducer";
 
-Bugsnag.start({
-  apiKey: "db3f0b9b86a138caccfb702fd123a187",
-  plugins: [new BugsnagPluginReact()],
-});
-
 export const store = createStore(rootReducer);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
