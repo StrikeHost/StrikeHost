f2f2d5bfabbb2c9ed3bc7087e95404009bbc74a5
Sam J Gunner <me@sgunner.me>
2023-02-22 13:06:20 +0000

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: [
+        'http://localhost:3000',
+        /\.strike.host$/,
+      ],
     },
   });
   app.setGlobalPrefix('api');
